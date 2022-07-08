ca58cae0042fc6e5979bb9184ffde446cf801246
timw5 <70343494+timw5@users.noreply.github.com>
2022-07-07 23:22:16 -0600

    added timewarp for hold, cleaned up some formatting,

--- StockTradingApp/Pages/Index.cshtml.cs
+++ StockTradingApp/Pages/Index.cshtml.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using StockTradingApp.Helpers;
 using System.Text.Json;
-using System.Web.Helpers;
 
 namespace StockTradingApp.Pages
 {
@@ -19,20 +18,23 @@ namespace StockTradingApp.Pages
 
 
         [BindProperty]
-        public List<string> SupportedTickers { get; set; }
+        public List<string>? SupportedTickers { get; set; }
         [BindProperty]
-        public string StartingDate { get; set; }
+        public string? StartingDate { get; set; }
         [BindProperty]
-        public int balance { get; set; }
+        public int? balance { get; set; }
         public void OnGet()
         {
             SupportedTickers = new();
             SupportedTickers = DataAccess.GetSupportedTickers();
+            
             StartingDate = DataAccess.GetRandomDate();
+            
             HttpContext.Session.Remove("date");
             HttpContext.Session.Remove("cents");
             HttpContext.Session.SetString("date", StartingDate);
             HttpContext.Session.SetInt32("cents", 1000000);
+            
             balance = 10000;
             
             
@@ -40,28 +42,77 @@ namespace StockTradingApp.Pages
         
         public IActionResult OnPostGetTickerData([FromBody]dynamic? data)
         {
-            var result= JsonSerializer.Deserialize<Dictionary<string, string>>(data);
-            string ticker = result["ticker"];
-            string date_ = HttpContext.Session.GetString("date");
-            string date = result["date"];
-            DataAccess da = new DataAccess(ticker);
-           var idx = da.BulkData.Where(x => x.date == date_ && x.ticker == ticker).Select(x => x.index).First();
-            var dat = da.BulkData.Where(x => x.index < idx && x.ticker == ticker);
-            return new JsonResult(dat);
+            if (HttpContext.Session.Get("date") is null || data is null)
+            {
+                return new JsonResult("");
+            }
+            try
+            {
+                var result = JsonSerializer.Deserialize<Dictionary<string, string>>(data);
+                string ticker = result["ticker"];
+                string date_ = HttpContext.Session.GetString("date");
+
+                DataAccess da = new DataAccess(ticker);
+                var idx = da.BulkData.Where(x => x.date == date_ && x.ticker == ticker).Select(x => x.index).First();
+                var dat = da.BulkData.Where(x => x.index < idx && x.ticker == ticker);
+
+                return new JsonResult(dat);
+            }
+            catch
+            {
+                return new JsonResult("");
+            }
+
+            
+
         }
         
         public IActionResult OnPostMinusFunds([FromBody]dynamic? data)
         {
-            var x = JsonSerializer.Deserialize<Dictionary<string, string>>(data);
-            var balance = HttpContext.Session.GetInt32("cents");
+            if(HttpContext.Session.Get("cents") is null || HttpContext.Session.Get("date") is null || data is null)
+            {
+                return new JsonResult("");
+            }
+            try
+            {
+                var x = JsonSerializer.Deserialize<Dictionary<string, string>>(data);
 
-            int newamnt = (int)(balance - int.Parse(x["amnt"]));
+                var date = HttpContext.Session.GetString("date");
+                var balance = HttpContext.Session.GetInt32("cents");
+
+                int newamnt = (int)(balance - int.Parse(x["amnt"]));
+                var dateFormat = DataAccess.Str_To_date(date);
+                var new_date = dateFormat.AddDays(7);
+
+                HttpContext.Session.SetString("date", new_date.ToString("yyyy-MM-dd"));
+                HttpContext.Session.SetInt32("cents", newamnt);
+
+                return new JsonResult(newamnt);
+            }
+            catch
+            {
+                return new JsonResult("");
+            }
+        }
+
+        public IActionResult OnPostTimewarp()
+        {
+            TimeWarp();
+            return new JsonResult("");
+
+        }
+        
+        private void TimeWarp()
+        {
+            if (HttpContext.Session.Get("date") is null)
+            {
+                return;
+            }
             var date = HttpContext.Session.GetString("date");
             var dateFormat = DataAccess.Str_To_date(date);
             var new_date = dateFormat.AddDays(7);
             HttpContext.Session.SetString("date", new_date.ToString("yyyy-MM-dd"));
-            HttpContext.Session.SetInt32("cents", newamnt);
-            return new JsonResult(newamnt);
+            
         }
 
 
